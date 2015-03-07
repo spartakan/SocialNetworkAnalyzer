c7f554e28b2166eac802dc769e9129c016d50646
spartakan <p.cruickshank@napier.ac.uk>
2015-03-07 22:13:34 +0000

    Basic changes to get the ocde running without error

---
+++ config.py
@@ -0,0 +1,50 @@
+import os
+import platform
+import sys
+import logging
+from debugging_setup import setup_logging, debug_print
+
+from SETTINGS import *
+
+#This file is included in every module/script
+
+#set system path
+if platform.system() == 'Windows':
+    if os.path.exists("~/SocialNetworkAnalyzer"):
+        #sys.path.append(os.path.abspath("~/SocialNetworkAnalyzer"))
+        sys.path.append(os.path.abspath(HOME_PATH))
+   
+elif platform.system() == 'Linux':
+    sys.path.insert(0,os.path.abspath(HOME_PATH))
+
+
+if platform.system() == "Windows":
+    if os.path.exists():
+        facebook_path_to_PAGES_FILE= ""
+        facebook_path_to_EXPORT_FILE= ""
+        twitter_path_to_EXPORT_FILE=  ""
+
+        debug_print("  Path exists: %s"%facebook_path_to_PAGES_FILE)
+
+    elif os.path.exists( ):
+        facebook_path_to_PAGES_FILE = ""
+        facebook_path_to_EXPORT_FILE = ""
+        twitter_path_to_EXPORT_FILE = ""
+        debug_print("  Path exists: %s" % facebook_path_to_PAGES_FILE)
+
+    else:
+        debug_print("  Path Not Found!")
+elif platform.system() == 'Linux':
+    facebook_path_to_PAGES_FILE =   ""   #official facebook pages 
+    facebook_path_to_EXPORT_FILE = ""
+    twitter_path_to_EXPORT_FILE = ""
+
+#Create a Resources folder and save the oauth file there!!!
+
+if platform.system() == 'Windows':
+    if os.path.exists("~/SocialNetworkAnalyzer/CrawlingModule/Resources/twitter_oauth.txt"):
+        twitter_OAUTH_FILE = os.path.expanduser("~/SocialNetworkAnalyzer/Resources/twitter_oauth.txt").replace("\\", "/")
+  
+elif platform.system() == 'Linux':
+    twitter_OAUTH_FILE = os.path.abspath(os.path.expanduser("~/SocialNetworkAnalyzer/Resources/twitter_oauth.txt"))
+

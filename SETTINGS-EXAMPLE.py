1e1a5827106715edadefa2120d9c04bf1b5604ce
spartakan <p.cruickshank@napier.ac.uk>
2015-04-08 21:07:14 +0100

    Some tidying up of settings & config: example settings file

---
+++ SETTINGS-EXAMPLE.py
@@ -0,0 +1,24 @@
+#Consumer key & secret for facebook authorization
+facebook_CONSUMER_ID    = "<your facebook consumer id here>"
+facebook_CONSUMER_SECRET = "<your facebook consumer secret here>"
+
+DEFAULT_MONGO_DB        = "twitter"
+DEFAULT_TWITTER_USER    = "xxx"
+DEFAULT_TWITTER_LIST    = "listname"
+
+#Consumer key & secret from https://apps.twitter.com/ for twitter authorization
+twitter_CONSUMER_KEY    = '<your key>'
+twitter_CONSUMER_SECRET =  '<your key>'
+twitter_oauth_token     = '<your key>'
+twitter_oauth_secret    = '<your key>'
+
+twitter_path_to_EXPORT_FILE = ""
+
+#determine path for files based on the computer you're using
+facebook_path_to_PAGES_FILE = ""  #list with pages for community councils
+facebook_path_to_EXPORT_FILE = ""
+
+#ONLY FOR WINDOWS
+#path_to_graph_file = '' #where to export the .gml file for windows
+HOME_PATH = '~/git/SocialNetworkAnalyzer'
+
